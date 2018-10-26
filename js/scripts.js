7de3d7d9913cbf2b2c7870dd3a599f494ab049b2
Kristin Brewer-Lowe <kristin.lowe1@gmail.com>
2018-10-26 09:59:01 -0700

    Add name field

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
 // Business Logic
-function createNumberList(number) {
+function createNumberList(number, name) {
   var numberList = [];
   for (var i = 0; i <= number; i++) {
     var numbersInList = (i.toString()).split("");
     if (i % 3 === 0 && i !== 0) {
-      numberList.push("I'm sorry [name]. I'm afraid I can't do that.")
+      numberList.push("I'm sorry " + name + ". I'm afraid I can't do that.")
     } else if (numbersInList.includes("1")) {
       numberList.push("Boop!")
     } else if (numbersInList.includes("0")) {
@@ -20,8 +20,9 @@ function createNumberList(number) {
 $(document).ready(function(){
   $("#beepBoop").submit(function(event) {
     event.preventDefault();
+    var name = $("input#name").val();
     var number = parseInt($("input#number").val());
-    var results = createNumberList(number);
+    var results = createNumberList(number, name);
     console.log(results);
     $("#results").show();
     results.forEach(function(result) {
